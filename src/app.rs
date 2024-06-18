e9e82a6d0a0bd3f1ae7d3ba1bcb3bb615b010ff7
G Pershing <gpershing1@gmail.com>
2024-06-17 17:08:50 -0700

    cleanup some boilerplate

--- src/app.rs
+++ src/app.rs
@@ -19,11 +19,6 @@ const SETTINGS_KEY: &str = "swap_settings";
 impl App {
     /// Called once before the first frame.
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        // This is also where you can customize the look and feel of egui using
-        // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
-
-        // Load previous app state (if any).
-        // Note that you must enable the `persistence` feature for this to work.
         let puzzle = cc.storage
             .and_then(|storage| eframe::get_value(storage, PUZZLE_KEY))
             .unwrap_or_else(|| PlayingPuzzle::play(fallback_puzzle()));
@@ -74,14 +69,8 @@ impl eframe::App for App {
 
     /// Called each time the UI needs repainting, which may be many times per second.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        // Put your widgets into a `SidePanel`, `TopBottomPanel`, `CentralPanel`, `Window` or `Area`.
-        // For inspiration and more examples, go to https://emilk.github.io/egui
-
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            // The top panel is often a good place for a menu bar:
-
             egui::menu::bar(ui, |ui| {
-                // NOTE: no File->Quit on web pages!
                 let is_web = cfg!(target_arch = "wasm32");
                 if !is_web {
                     ui.menu_button("File", |ui| {
@@ -89,22 +78,20 @@ impl eframe::App for App {
                             ctx.send_viewport_cmd(egui::ViewportCommand::Close);
                         }
                     });
-                    ui.add_space(16.0);
                 }
+
                 if ui.button("How to play...").clicked() {
                     self.showing_tutorial = true;
                 }
-                if ui.button("Custom generator...").clicked() {
+                if ui.button("Generator...").clicked() {
                     self.editing_generator_settings = true;
                 }
 
                 egui::widgets::global_dark_light_mode_buttons(ui);
             });
         });
-        egui::CentralPanel::default().show(ctx, |ui| {
-            // The central panel the region left after adding TopPanel's and SidePanel's
-            ui.heading("swap");
 
+        egui::CentralPanel::default().show(ctx, |ui| {
             ui.centered_and_justified(|ui| {
                 let response = update_game(ui, &mut self.puzzle, &mut self.game_state, &mut self.puzzle_state, &GameStyle {
                     scale: 150.0
@@ -141,6 +128,8 @@ fn powered_by_egui_and_eframe(ui: &mut egui::Ui) {
             "eframe",
             "https://github.com/emilk/egui/tree/master/crates/eframe",
         );
+        ui.label(". View the ");
+        ui.hyperlink_to("source", "https://github.com/gpershing/swap_wasm");
         ui.label(".");
     });
 }
